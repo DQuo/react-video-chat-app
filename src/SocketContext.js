daa065f6397b4359c32ecb26adbdba49be9b77a6
Deonte Waller <dwallerofficial@gmail.com>
2021-07-22 02:08:07 -0400

    Socket Context Provider setup.

---
+++ src/SocketContext.js
@@ -0,0 +1,23 @@
+import React, { createContext, useState, useRef, useEffect } from 'react';
+import { io } from 'socket.io-client';
+import Peer from 'simple-peer';
+
+const SocketContext = createContext();
+
+const socket = io('http://localhost:5000');
+
+const ContextProvider = ({children}) => {
+  const [stream, setStream] = useState(null);
+
+  useEffect(() => {
+    navigator.mediaDevices.getUserMedia({});
+  })
+  
+  const answerCall = () => {
+
+  }
+
+  const callUser = () => {
+
+  }
+}
